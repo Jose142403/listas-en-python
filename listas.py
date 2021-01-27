e999072495fb347aeb55e2d22af134029f762a63
Jose David Gaviria Rendon <josedavidinglesamericano@gmail.com>
2021-01-26 19:33:47 -0600

    ntento1

--- listas.py
+++ listas.py
@@ -2,6 +2,11 @@ import time;
 lista1=[5,4,3,2,10];
 lista2=[8,7,6,5,4,3,2,1,0]
 lista=[lista1,lista2];
-print(lista[0[0]])
+h=lista[0];
+print(h);
+lista1[0]=3;
+print(lista1)
+print(lista1[-1]);
+print(lista1.length())
 time.sleep(50);
 
